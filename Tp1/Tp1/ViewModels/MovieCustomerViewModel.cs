72ac46550fb7bd3c717881deadaf49e73e84c145
Olfa Medimegh <olfa.medimegh@insat.ucar.tn>
2023-10-09 11:02:19 +0100

    First push

---
+++ Tp1/Tp1/ViewModels/MovieCustomerViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Tp1.Models;
+
+namespace Tp1.ViewModels
+{
+    public class MovieCustomerViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
