0f3831d359ae6fcb25fbcae7fb438383de67d3ae
Olfa Medimegh <olfa.medimegh@insat.ucar.tn>
2023-12-02 23:35:46 +0100

    .

--- LearningProject/LearningProject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace LearningProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
